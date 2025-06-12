c689474359f4326b0696c0a654a69dfe9d3961e8
Virginia <vrebeccagrant@gmail.com>
2025-06-12 16:34:57 -0400

    enginesand3rdpartmiddleware

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ const app = express();
 
 app.listen(3000);
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => { 
+    const blogs = [
+    //{title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //{title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //{title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+
+  res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {title:'About'});
+});
+
+app.get('/blogs/create' , (req, res) => {
+    res.render('create', {title:'Create a new blog'});
+});
+
+app.use((req,res) => {
+    res.render('404', { title: '404' });
+})
+
+
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
 app.get('/', (req,res) =>{
     //res.send('<p>home page</p>')
     res.sendFile('./views/index.html', {root: __dirname});
